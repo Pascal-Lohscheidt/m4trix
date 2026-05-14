eebb7d25972c43278e11ad4b27f6b9b8e0bf6724
Pascal Lohscheidt <pascal.lohscheidt@gmail.com>
2026-05-14 15:52:03 +0200

    refactor(traceer): added adapter to offer different tracer shapes

--- packages/tracing/tsup.config.ts
+++ packages/tracing/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: {
     index: 'src/index.ts',
-    'adapters/langgraph': 'src/adapters/langgraph.ts',
   },
   format: ['esm', 'cjs'],
   dts: true,
